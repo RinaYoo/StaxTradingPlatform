9716ffeff3a896319e6e88d6c382298ef0cc72ef
RinaYoo <62081885+RinaYoo@users.noreply.github.com>
2022-10-30 05:26:26 +1100

    some css changed

--- src/components/CoinItems.js
+++ src/components/CoinItems.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FiArrowUpRight, FiArrowDown } from 'react-icons/fi'
 
 // My Styles
 import '../styles/coins.css';
@@ -12,7 +13,20 @@ const CoinItems = (props) => {
         <p>{props.coins.symbol.toUpperCase()}</p>
       </div>
       <p>${props.coins.current_price.toLocaleString()}</p>
-      <p>{props.coins.price_change_percentage_24h.toFixed(2)}%</p>
+
+
+      {props.coins.price_change_percentage_24h < 0 ? (
+        <span className='red'>
+          <FiArrowDown />
+          {props.coins.price_change_percentage_24h.toFixed(2)}%
+        </span>
+      ) : (
+        <span className='green'>
+          <FiArrowUpRight />
+          {props.coins.price_change_percentage_24h.toFixed(2)}%
+        </span>
+      )}
+
       <p>${props.coins.total_volume.toLocaleString()}</p>
       <p>${props.coins.market_cap.toLocaleString()}</p>
     </div>
