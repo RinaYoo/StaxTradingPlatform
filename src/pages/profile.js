4e58be5d94595c301c830cf89a5470c5dfa5f3ee
Ninghan <ninghan1008@gmail.com>
2022-10-30 17:21:25 +0800

    profile

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth, signOut ,updatePassword} from "firebase/auth";
+import { Box } from "@mui/system";
+import { TextField,Button,IconButton,InputAdornment,OutlinedInput,FormControl,InputLabel } from "@mui/material";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { useHistory } from "react-router-dom";
 
 function Profile() {
   
   const auth = getAuth();
-
+  let navigate = useHistory();
   const handleLogout = (e) => {
     signOut(auth)
       .then(() => {
@@ -16,6 +21,37 @@ function Profile() {
         console.log("failed");
       });
   };
+  const [values, setValues] = React.useState({
+    password: '' ,
+    showPassword: false,
+  });
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+
+  const submit = ()=>{
+    if(!values.password){
+     alert("Empty value not allowed");
+     return; 
+    }
+    updatePassword(auth.currentUser,values.password).then(()=>{
+      alert("update password successfully!");
+      navigate.push("/");
+    }).catch((err)=>{
+      alert("Fail to update password: "+err);
+    })
+  }
 
   return (
     <div>
@@ -32,6 +68,48 @@ function Profile() {
 
       <section>
         <h1>My Profile</h1>
+        <div style={{marginTop:"40px"}}>
+          <Box
+            sx={{
+              width: 500,
+              maxWidth: '100%',
+            }}
+          >
+            <TextField fullWidth label="E-mail" type="email" defaultValue={JSON.parse(localStorage.getItem("user"))['email']} disabled />
+          </Box>
+          <br></br>
+          <Box
+            sx={{
+              width: 500,
+              maxWidth: '100%',
+            }}
+          >
+            <FormControl sx={{ width: '100%' }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">New Password</InputLabel>
+            <OutlinedInput 
+              id="outlined-adornment-password"
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={handleChange('password')}
+              endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+          </Box>
+          <br></br>
+          <Button variant="contained" onClick={submit}>Submit</Button>
+        </div>
 
       </section>
     </div>
